357703b4191bcfd34b897d7284b5af30b3502852
Johnsoy.zhao <15520754767@163.com>
2025-06-22 10:33:02 +0800

    feat(utils): 新增 NamedThreadFactory 类
    实现了 NamedThreadFactory 接口，用于创建具有指定名称前缀和守护状态的线程。- 添加了类的成员变量和构造方法
    - 实现了 newThread 方法，根据给定的 Runnable 对象创建并返回一个线程
    - 线程名称使用前缀和递增的数字组合，守护状态根据构造方法参数设置

--- src/main/java/org/example/utils/NamedThreadFactory.java
+++ src/main/java/org/example/utils/NamedThreadFactory.java
@@ -1,4 +1,23 @@
 package org.example.utils;
 
-public class NamedThreadFactory {
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class NamedThreadFactory implements ThreadFactory {
+
+    private final String namePrefix;
+    private final boolean daemon;
+    private final AtomicLong threadNumber = new AtomicLong(0);
+
+    public NamedThreadFactory(String namePrefix, boolean daemon) {
+        this.namePrefix = namePrefix;
+        this.daemon = daemon;
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread thread = new Thread(r, namePrefix + "-" + threadNumber.incrementAndGet());
+        thread.setDaemon(daemon);
+        return thread;
+    }
 }
